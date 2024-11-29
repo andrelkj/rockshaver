65b2180781f7d09f2ce1a932cd9e413e9dfb7d28
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-11-28 23:42:31 -0300

    feature: refactor login test suite to the update pre-registration feature

--- web/cypress/e2e/ pre-registration.cy.js
+++ web/cypress/e2e/ pre-registration.cy.js
@@ -1,13 +1,12 @@
-describe("Login", () => {
-  it("Should login successfully", () => {
+describe("Pre-registration", () => {
+  it("Should perform clients pre-registration", () => {
     cy.visit("/");
 
-    cy.get('header nav a[href="entrar"]').click();
+    cy.get('header nav a[href="pre-cadastro"]').click();
     cy.get("form h2").should("be.visible").and("have.text", "Seus dados");
 
-    cy.get('input[placeholder="Nome"]').type('Customer Test')
-    cy.get('input[placeholder="E-mail"]').type('customer@test.com')
-    cy.get('input[placeholder="Whatsapp"]').type('11999999999')
+    cy.get('input[name="nome"]').type('Customer Test')
+    cy.get('input[name="email"]').type('customer@test.com')
 
     cy.contains('button[type="submit"]', 'Continuar').click()
     cy.get('.user-name').should('be.visible').and('have.text', 'Customer Test')
