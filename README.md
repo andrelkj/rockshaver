555ac9d85263162bba159ebed0098f359ee3bdf5
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-12-02 00:57:05 -0300

    test: add pre-registration test suite for valid and invalid field inputs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The project is structured to use a docker comntainer that contains the api, web 
 
 ### Installation and configuration
 
-- In order to use the existent docker images we'll first need to install [Docker Desktop](https://www.docker.com/) to your desired OS. 
+- In order to use the existent docker images we'll first need to install [Docker Desktop](https://www.docker.com/) to your desired OS.
 - Once it is installed we also need to instantiate the remote containers to our local machine by running the terminal command `docker compose up -d`
 - Web and mobile applications runs locally, respectivelly, against `localhost:3000` and `localhost:8100` ports while the database manager runs against `localhost:17017` port
 
@@ -15,3 +15,19 @@ The project is structured to use a docker comntainer that contains the api, web 
 It is also posible to remove docker containers and images running the terminal `docker compose down` command.
 
 **Note:** be aware that this command will remove containers, images, and any storage (e.g. database information). Once executed existent storage is lost and you'll need to compose a fresh container to use it's images again.
+
+## Best practices
+
+### Filtering locators by visual identification
+
+One common approach to finding complex locators in test automation is to use XPath which provide a precise way to finding DOM elements (e.g. `//label[text()="Nome Completo"]/..//div[contains(@class,"alert-msg")]`), although Cypress doesn't provide support to XPath and even though there might be external libraries, extensions or plugins that can fullfil this need theres an alternative and build in approach in Cypress that is a better fit using the `parent()` function:
+
+```js
+cy.contains('label', 'Nome Completo')
+  .parent()
+  .find('.alert-msg')
+  .should('be.visible')
+  .and('include.text', 'O campo nome é obrigatório')
+```
+
+**Note:** visual identification method focused on finding locators per visual snipets (as you would in a manual test)
--- web/cypress/e2e/ pre-registration.cy.js
+++ web/cypress/e2e/ pre-registration.cy.js
@@ -5,11 +5,70 @@ describe("Pre-registration", () => {
     cy.get('header nav a[href="pre-cadastro"]').click();
     cy.get("form h2").should("be.visible").and("have.text", "Seus dados");
 
-    cy.get('input[name="full-name"]').type('Customer Test')
-    cy.get('input[name="email"]').type('customer@test.com')
+    cy.get('input[name="full-name"]').type("Customer Test");
+    cy.get('input[name="email"]').type("customer@test.com");
 
-    cy.contains('button[type="submit"]', 'Continuar').click()
-    cy.get('.user-name').should('be.visible').and('have.text', 'Olá, Customer')
-    cy.get('.user-email').should('be.visible').and('have.text', 'customer@test.com')
+    cy.contains('button[type="submit"]', "Continuar").click();
+    cy.get(".user-name").should("be.visible").and("have.text", "Olá, Customer");
+    cy.get(".user-email")
+      .should("be.visible")
+      .and("have.text", "customer@test.com");
+  });
+
+  it("Required fields", () => {
+    cy.visit("/");
+
+    cy.get('header nav a[href="pre-cadastro"]').click();
+    cy.get("form h2").should("be.visible").and("have.text", "Seus dados");
+
+    cy.contains('button[type="submit"]', "Continuar").click();
+
+    cy.contains("label", "Nome Completo")
+      .parent()
+      .find(".alert-msg")
+      .should("be.visible")
+      .and("have.text", "O campo nome é obrigatório.");
+
+    cy.contains("label", "E-mail")
+      .parent()
+      .find(".alert-msg")
+      .should("be.visible")
+      .and("have.text", "O campo e-mail é obrigatório.");
+  });
+
+  it("Should not complete pre-registration with the first name only", () => {
+    cy.visit("/");
+
+    cy.get('header nav a[href="pre-cadastro"]').click();
+    cy.get("form h2").should("be.visible").and("have.text", "Seus dados");
+
+    cy.get('input[name="full-name"]').type("Customer");
+    cy.get('input[name="email"]').type("customer@test.com");
+
+    cy.contains('button[type="submit"]', "Continuar").click();
+
+    cy.contains("label", "Nome Completo")
+      .parent()
+      .find(".alert-msg")
+      .should("be.visible")
+      .and("have.text", "Informe seu nome completo.");
+  });
+
+  it("Should not complete pre-registration with invalid email", () => {
+    cy.visit("/");
+
+    cy.get('header nav a[href="pre-cadastro"]').click();
+    cy.get("form h2").should("be.visible").and("have.text", "Seus dados");
+
+    cy.get('input[name="full-name"]').type("Customer User");
+    cy.get('input[name="email"]').type("www.customertest.com");
+
+    cy.contains('button[type="submit"]', "Continuar").click();
+
+    cy.contains("label", "E-mail")
+      .parent()
+      .find(".alert-msg")
+      .should("be.visible")
+      .and("have.text", "O e-mail inserido é inválido.");
   });
 });
