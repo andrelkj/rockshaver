2ed893d26cebf4b2d136f5530a049ff00fc07094
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2025-01-18 17:38:37 -0300

    feat: add API integration for agendamento and add test cases for duplicate slots

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -25,7 +25,7 @@ describe("Agendamento", () => {
     cy.verificarPreCadastro(agendamento.usuario);
 
     cy.iniciarAgendamento();
-    cy.escolherProfissional(agendamento.profissional);
+    cy.escolherProfissional(agendamento.profissional.nome);
     cy.selecionarServico(agendamento.servico.descricao);
     cy.escolherDiaDoAgendamento(agendamento.dia);
     cy.escolherHorarioDoAgendamento(agendamento.hora);
@@ -34,5 +34,36 @@ describe("Agendamento", () => {
     cy.get("h3")
       .should("be.visible")
       .and("have.text", "Tudo certo por aqui! Seu horário está confirmado.");
+  });
+
+  it.only("Deve deve mostrar o slot ocupada", function () {
+    const agendamento = this.agendamentos.duplicado;
+
+    cy.dropCollection("agendamentos", {
+      failSilently: "true",
+    }).then((result) => {
+      cy.log(result);
+    });
+
+    cy.agendamentoAPI(agendamento);
+
+    cy.intercept("GET", "http://localhost:3333/api/calendario", {
+      statusCode: 200,
+      body: calendario,
+    }).as("getCalendario");
+
+    cy.iniciarPreCadastro(agendamento.usuario);
+    cy.verificarPreCadastro(agendamento.usuario);
+
+    cy.iniciarAgendamento();
+    cy.escolherProfissional(agendamento.profissional.nome);
+    cy.selecionarServico(agendamento.servico.descricao);
+    cy.escolherDiaDoAgendamento(agendamento.dia);
+
+    cy.get(`[slot="${agendamento.hora} - ocupado"`)
+      .should("be.visible")
+      .find("svg")
+      .should("be.visible")
+      .and("have.css", "color", "rgb(255, 255, 255)");
   });
 });
